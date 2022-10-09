f852e3d35797cf0f5889fa6ef23d2cf260d30832
Ana Borges <avb5bt@virginia.edu>
2022-10-09 13:14:17 -0400

    made successful test

--- study_buddy_app/tests.py
+++ study_buddy_app/tests.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
-
-# Create your tests here.
+class UserModelTests(TestCase):
+    def test_user_created(self):
+        """
+        exists() returns True when a user was successfully created
+        """
+        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        user.last_name = 'Lennon'
+        user.save()
+        self.assertIs(User.objects.filter(username='john').exists(), True)
