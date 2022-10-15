d159349c78f212680577e392be886d6ae2f53d56
Bao Nguyen <bkn5rk@virginia.edu>
2022-10-15 17:06:15 -0400

    added chat app
    Ana + Bao

--- study_buddy_app/urls.py
+++ study_buddy_app/urls.py
@@ -6,9 +6,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='sign_in'),
     path('sign_in/', views.sign_in, name='sign_in'),
     path("accounts/", include("allauth.urls")),
     path('logout', LogoutView.as_view()),
+
+
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+
+
     # ex: /polls/5/
     # path('<int:question_id>/', views.detail, name='detail'),
     # # ex: /polls/5/results/
--- study_buddy_app/views.py
+++ study_buddy_app/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from study_buddy_app.models import Room, Message
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
 def index(request):
@@ -11,6 +13,46 @@ def sign_in(request):
     template = loader.get_template('study_buddy_app/google_login.html')
     context = {}
     return HttpResponse(template.render(context, request))
+def home(request):
+    return render(request, 'chat.html')
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
+    return render(request, 'room.html', {
+        'username': username,
+        'room': room,
+        'room_details': room_details
+    })
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
+
+
+
 
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
