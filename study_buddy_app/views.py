77d0c1e7a1fafa85c1784085f2a5f20390ca003c
evanjtierney <112533217+evanjtierney@users.noreply.github.com>
2022-10-15 17:23:16 -0400

    attempt1_TemplateDoesNotExist_error

--- study_buddy_app/views.py
+++ study_buddy_app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+import requests
 
 def index(request):
     template = loader.get_template('study_buddy_app/home.html')
@@ -11,6 +13,10 @@ def sign_in(request):
     template = loader.get_template('study_buddy_app/google_login.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def api(request):
+    response = requests.get('http://luthers-list.herokuapp.com/api/deptlist/?format=json').json()
+    return render(request, 'api.html', {'response':response})
 
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
