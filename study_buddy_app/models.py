0aca129a9b361accfc49176f954a802f4da8630b
Bao Nguyen <bkn5rk@virginia.edu>
2022-11-04 23:06:21 -0400

    adding slugs
    auto pop slugs and auto save to keep up to date with usernames

--- study_buddy_app/models.py
+++ study_buddy_app/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.dispatch import receiver #add this
 from django.db.models.signals import post_save #add this
+from django.template.defaultfilters import slugify  # new
+
 class Room(models.Model):
     name = models.CharField(max_length=1000)
 class Message(models.Model):
@@ -15,11 +17,18 @@ class Message(models.Model):
 class Profile(models.Model):  # add this class and the following fields
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = self.user.username
+        super(Profile, self).save(*args, **kwargs)
+
+
     @receiver(post_save, sender=User)  # add this
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
+
 
     @receiver(post_save, sender=User)  # add this
     def save_user_profile(sender, instance, **kwargs):
