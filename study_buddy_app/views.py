a82bddfbf11dca00ced40bcde60e00fa0418bec2
evanjtierney <112533217+evanjtierney@users.noreply.github.com>
2022-11-13 15:26:02 -0500

    classes visible in public profile

--- study_buddy_app/views.py
+++ study_buddy_app/views.py
@@ -229,7 +229,7 @@ class seeProfile(generic.DetailView):
     model = Profile
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs) 
         return context
     
 def user_redirect(request):
