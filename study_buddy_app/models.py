7ec6cd17d3a114d66ca4846df5f9f246494d3a78
Ana Borges <avb5bt@virginia.edu>
2022-11-16 23:39:55 -0500

    added Event to models

--- study_buddy_app/models.py
+++ study_buddy_app/models.py
@@ -68,3 +68,9 @@ class FriendRequest(models.Model):
 ##    from_user = models.ForeignKey(User, related_name='from_user', on_delete=models.CASCADE)
 ##    to_user = models.ForeignKey(User, related_name='to_user', on_delete=models.CASCADE)
 ##
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
